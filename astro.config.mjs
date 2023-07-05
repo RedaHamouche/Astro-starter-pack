4365f162f063944ffdd35d0b47c76cdb782e54cb
RedaHamouche <reda.hamouche@outlook.com>
2023-07-05 17:29:45 +0200

    added netlift adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import nodejs from '@astrojs/node';
+import node from "@astrojs/node";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   // ...
+  adapter: netlify(),
+  output: 'hybrid',
   site: 'https://astro-starter-pack.netlify.app',
   styles: [{
     // Apply Tailwind CSS
